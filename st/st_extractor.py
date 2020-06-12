5ae7592fe59d15213e178be016330e73524a996d
Jake Ross <jirhiker@gmail.com>
2020-06-12 11:57:59 -0600

    fixed st

--- st/st_extractor.py
+++ st/st_extractor.py
@@ -76,7 +76,7 @@ def st_upload(input_file):
     with open(input_file, 'r') as rfile:
 
         try:
-            yd = yml.load(rfile, Loader=yml.SafeLoader)
+            yd = yaml.load(rfile, Loader=yaml.SafeLoader)
         except BaseException as e:
             yd = None
             print('asdf', e)
